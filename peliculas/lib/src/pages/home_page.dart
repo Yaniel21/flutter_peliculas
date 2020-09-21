9557f974f77332d3e8c9295d46cf00339657b4ca
majediver <majevski.nestor.gabriel@gmail.com>
2020-09-20 22:59:54 -0300

    scroll

--- peliculas/lib/src/pages/home_page.dart
+++ peliculas/lib/src/pages/home_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
+//import 'dart:html';
 
 class HomePage extends StatelessWidget {
+  final peliculasProvider = new PeliculasProvider();
+  //intento por resolver
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,18 +23,52 @@ class HomePage extends StatelessWidget {
         ),
         body: Container(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
               _swiperTarjetas(),
+              _footer(context),
             ],
           ),
         ));
   }
 
   Widget _swiperTarjetas() {
-    final peliculasProvider = new PeliculasProvider();
-    peliculasProvider.getEnCines();
-    return CardSwiper(
-      peliculas: [1, 2, 4, 5, 6],
+    return FutureBuilder(
+      future: peliculasProvider.getEnCines(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return CardSwiper(peliculas: snapshot.data);
+        } else {
+          return Container(
+              height: 300.0, child: Center(child: CircularProgressIndicator()));
+        }
+      },
+    );
+  }
+
+  Widget _footer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start ,
+        children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(left: 20.0 ),
+          child: Text('Populares', style: Theme.of(context).textTheme.subtitle1)),
+        SizedBox(height: 5.0),
+        FutureBuilder(
+          future: peliculasProvider.getPopulares(),
+          builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+            if (snapshot.hasData) {
+              return MovieHorizontal(peliculas: snapshot.data);
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+
+        //Text('Populares', style: Theme.of(context).textTheme.headline6),
+      ]),
     );
   }
 }
